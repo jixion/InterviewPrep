09c6481bb769eb6ef7f3200d76b7a71396a1cbbd
Jeff Burroughs <jburroughs@sparkimaginations.com>
2016-11-02 21:00:51 -0400

    node setup

---
+++ linkedList/node/node.service.js
@@ -0,0 +1,27 @@
+(function () {
+
+    function Node () {
+        var node = this;
+        node.data = {};
+        node.next = function () {};
+
+        node.setData = function (data) {
+            node.data = data;
+        };
+
+        node.getData = function () {
+            return node.data;
+        };
+
+        node.setNext = function (nodePointer) {
+            node.next = nodePointer;
+        }
+
+        node.getNext = function () {
+            return node.next;
+        }
+    }
+
+    angular.module('node')
+        .service('Node', Node);
+})();
