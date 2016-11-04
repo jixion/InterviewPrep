76000aab0ba53a055e9ce954b9fb5569ab74666e
Jeff Burroughs <jburroughs@sparkimaginations.com>
2016-11-03 22:49:10 -0400

    Mulitply without using asterisk unary operator

---
+++ multiply/src/Multiply.java
@@ -0,0 +1,17 @@
+/**
+ * Created by jeff on 11/3/16.
+ */
+
+public class Multiply {
+    public static void main(String[] args) {
+        int one = 6;
+        int two = 5;
+        int total = 0;
+        for(int i = 0; i < two; i++) {
+            total = total + one;
+        }
+
+        System.out.println(total);
+    }
+}
+
