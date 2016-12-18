51abc7e23a3d8f0384244fb2549528efc887e420
Jeff Burroughs <jburroughs@sparkimaginations.com>
2016-12-18 10:40:50 -0500

    isInfiniteProcess

--- codeFights/codeFights.js
+++ codeFights/codeFights.js
@@ -84,3 +84,23 @@ function knapsackLight(value1, weight1, value2, weight2, maxW) {
 function extraNumber(a, b, c) {
     return a == c ? b : (b == c ? a : c);
 }
+
+/**
+ Given integers a and b, determine whether the following pseudocode results in an infinite loop
+
+ while a is not equal to b do
+ increase a by 1
+ decrease b by 1
+ Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+
+ Example
+
+ For a = 2 and b = 6, the output should be
+ isInfiniteProcess(a, b) = false;
+ For a = 2 and b = 3, the output should be
+ isInfiniteProcess(a, b) = true.
+ */
+
+function isInfiniteProcess(a, b) {
+    return a > b ? true : Math.abs(a - b) % 2 != 0 ;
+}
