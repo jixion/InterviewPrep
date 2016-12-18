15537946746e2325ba16a95d919af745d25652bd
Jeff Burroughs <jburroughs@sparkimaginations.com>
2016-12-18 10:45:24 -0500

    arithmeticExpression

--- codeFights/codeFights.js
+++ codeFights/codeFights.js
@@ -104,3 +104,32 @@ function extraNumber(a, b, c) {
 function isInfiniteProcess(a, b) {
     return a > b ? true : Math.abs(a - b) % 2 != 0 ;
 }
+
+/**
+ Consider an arithmetic expression of the form A#B=C. Check whether it is possible to replace # with one of the four signs: +, -, * or / to obtain a correct expression.
+
+ Example
+
+ For A = 2, B = 3 and C = 5, the output should be
+ arithmeticExpression(A, B, C) = true.
+
+ We can replace # with a + to obtain 2 + 3 = 5, so the answer is true.
+
+ For A = 8, B = 2 and C = 4, the output should be
+ arithmeticExpression(A, B, C) = true.
+
+ We can replace # with a / to obtain 8 / 2 = 4, so the answer is true.
+
+ For A = 8, B = 3 and C = 2, the output should be
+ arithmeticExpression(A, B, C) = false.
+
+ 8 + 3 = 11 ≠ 2;
+ 8 - 3 = 5 ≠ 2;
+ 8 * 3 = 24 ≠ 2;
+ 8 / 3 = 2.(6) ≠ 2.
+ So the answer is false.
+ */
+
+function arithmeticExpression(A, B, C) {
+    return (A+B==C) || (A-B==C) || (A*B==C) || (A/B==C);
+}
